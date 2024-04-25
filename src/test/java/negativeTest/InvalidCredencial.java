86a75ea74b5c32f0fa29ed9756aacf79ad27d942
Saiful8183 <Manha103090@gmail.com>
2024-04-25 00:47:07 -0400

    NegetiveTestCases

---
+++ src/test/java/negativeTest/InvalidCredencial.java
@@ -0,0 +1,129 @@
+package negativeTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class InvalidCredencial {
+
+	WebDriver driver;
+
+	@Parameters("browser")
+	@BeforeTest
+	public void setup(String browser) {
+
+		if (browser.equalsIgnoreCase("chrome")) {
+
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+
+		} else if (browser.equalsIgnoreCase("edge")) {
+
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+
+		} else if (browser.equalsIgnoreCase("firefox")) {
+
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+
+		}
+
+	}
+
+	@Test
+	public void TestPositiveTestCases() {
+
+		System.setProperty("webdriver.Edge.driver",
+				"C:\\Users\\Manha\\OneDrive\\Desktop\\Selenium&Driver\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.saucedemo.com/v1/index.html");
+		WebElement username = driver.findElement(By.xpath("//input[@id='user-name']"));
+		username.sendKeys("standard_user");
+		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+		password.sendKeys("secret_sauce");
+
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+		Assert.assertTrue(driver.findElement(By.className("product_label")).isDisplayed());
+
+		driver.close();
+	}
+
+	@Test
+	public void TestNegetiveTestCasesss() {
+
+		System.setProperty("webdriver.Edge.driver",
+				"C:\\Users\\Manha\\OneDrive\\Desktop\\Selenium&Driver\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.saucedemo.com/v1/index.html");
+		WebElement username = driver.findElement(By.xpath("//input[@id='user-name']"));
+		username.sendKeys("standard_us");
+		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+		password.sendKeys("secret_sau");
+
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+
+		Assert.assertTrue(
+				driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/h3")).isDisplayed());
+
+		driver.close();
+	}
+
+	@Test
+	public void TestNegetiveTestCasess() {
+
+		System.setProperty("webdriver.Edge.driver",
+				"C:\\Users\\Manha\\OneDrive\\Desktop\\Selenium&Driver\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.saucedemo.com/v1/index.html");
+		WebElement username = driver.findElement(By.xpath("//input[@id='user-name']"));
+		username.sendKeys("standard_user");
+		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+		password.sendKeys("secret_sau");
+
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+
+		Assert.assertTrue(
+				driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/h3")).isDisplayed());
+
+		driver.close();
+	}
+
+	@Test
+	public void TestNegetiveTestCases() {
+
+		System.setProperty("webdriver.Edge.driver",
+				"C:\\Users\\Manha\\OneDrive\\Desktop\\Selenium&Driver\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.saucedemo.com/v1/index.html");
+		WebElement username = driver.findElement(By.xpath("//input[@id='user-name']"));
+		username.sendKeys("standard_us");
+		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+		password.sendKeys("secret_sauce");
+
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+
+		Assert.assertTrue(
+				driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/h3")).isDisplayed());
+
+		driver.close();
+	}
+
+}
